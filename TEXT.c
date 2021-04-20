0678fd67f7542aac29a5bb498e9c7839164999fa
wyb-0901 <528198433@qq.com>
2021-04-20 10:18:11 +0800

    简单测试。

---
+++ TEXT.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	double b = 0;
+	char c = 0;
+	float d = 0;
+	long e = 0;
+	short f = 0;
+	printf("%d\n", sizeof(a));
+	printf("%d\n", sizeof(b));
+	printf("%d\n", sizeof(c));
+	printf("%d\n", sizeof(d));
+	printf("%d\n", sizeof(e));
+	printf("%d\n", sizeof(f));
+	getchar();
+	return 0;
+}
